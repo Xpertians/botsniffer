2fa040532ea5d14fcebf799dbb6142bfb307faa0
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2023-04-14 11:54:36 -0700

    add repetitive patterns example

--- sniffer/feature_extraction/feature_extraction.py
+++ sniffer/feature_extraction/feature_extraction.py
@@ -3,7 +3,7 @@ from .comments import analyze_comment_quality
 from .indentation import calculate_indentation_consistency
 from .style_guide import calculate_style_guide_adherence
 from .patterns import detect_repetitive_patterns
-#from .complexity import calculate_code_complexity
+from .complexity import calculate_code_complexity
 
 def extract_features(file_path, parsed_code):
     # Dict with features
@@ -14,6 +14,7 @@ def extract_features(file_path, parsed_code):
     features["code_identation"] = calculate_indentation_consistency(file_path, parsed_code)
     features["style_adherence"] = calculate_style_guide_adherence(file_path, parsed_code)
     features["repetitive_patterns"] = detect_repetitive_patterns(file_path, parsed_code)
+    features["code_complexity"] = detect_repetitive_patterns(file_path, parsed_code)
     
     print(features)
 

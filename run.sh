bb0f37793046bd3e6f815acdff4cbdef7706c2e2
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2023-04-15 19:42:35 -0700

    renaming the project

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf dist/*
+rm -rf build/
+pycodestyle --exclude='*testfiles*' . | grep -v 'build' | grep -v 'dist' | grep -v 'W605' > pep8.log
+python3 setup.py sdist bdist_wheel > install-pip.log
+pip3 uninstall botsniffer  -y >> install-pip.log
+pip3 install dist/botsniffer*.whl >> install-pip.log
+botsniffer ./
--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="codebot-sniffer", version="0.1",
-    description="codebot-sniffer - Detects AI generated source code using feature extraction with Machine Learning",
+    name="botsniffer", version="0.1",
+    description="botsniffer - Detects AI generated source code using feature extraction with Machine Learning",
     author="Oscar Valenzuela",
     author_email="oscar.valenzuela.b@gmail.com",
-    packages=['sniffer', 'sniffer.data'],
-    entry_points={"console_scripts": ["sniffer=sniffer.scanner:main"]},
+    packages=['botsniffer', 'botsniffer.data'],
+    entry_points={"console_scripts": ["botsniffer=botsniffer.scanner:main"]},
     install_requires=["ast-comments"],
     url='https://opensourcelicensecompliance.com',
     package_data={
-        "sniffer.data": ["*.txt", "*.ini", "*.sig"],
+        "botsniffer.data": ["*.txt", "*.ini", "*.sig"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",

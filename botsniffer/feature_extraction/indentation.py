04e92bc6f8a2e156b8ebb56c0b6657daf1e44a59
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2023-04-17 04:35:08 +0200

    moving identation to use simple approach

--- botsniffer/feature_extraction/indentation.py
+++ botsniffer/feature_extraction/indentation.py
@@ -1,40 +1,30 @@
 import ast
 import math
+import numpy as np
 
 
-def calculate_indentation_consistency(parsed_code):
+def calculate_indentation_consistency(parsed_code, indentation_size=4):
     # Calculates the indentation consistency of the parsed code.
     indentation_levels = []
     # Traverse the AST and collect the indentation levels of all nodes
     for node in ast.walk(parsed_code):
-        if isinstance(node, (
-                ast.FunctionDef,
-                ast.AsyncFunctionDef,
-                ast.ClassDef
-        )):
+        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)):
             # Exclude function and class definitions
             # from the indentation consistency calculation
             continue
-        if hasattr(node, "col_offset"):
-            # col_offset is the character offset
-            # of the node within its enclosing scope
-            indentation_level = node.col_offset // 4
-            indentation_levels.append(indentation_level)
-    # Calculate the standard deviation of the indentation levels
+        if not isinstance(node, ast.AST):
+            # Exclude non-AST nodes (such as comments)
+            continue
+        if not hasattr(node, "lineno"):
+            # Exclude nodes without a line number (such as the root Module node)
+            continue
+        indentation_level = node.col_offset // indentation_size
+        indentation_levels.append(indentation_level)
+    # Calculate the median absolute deviation (MAD) of the indentation levels
     if len(indentation_levels) <= 1:
         # If there is only one or zero indentation levels,
         # return perfect consistency
         return 1.0
     else:
-        return 1.0 - (
-            calculate_std_dev(indentation_levels) /
-            (len(indentation_levels) - 1)
-        )
-
-
-def calculate_std_dev(data):
-    n = len(data)
-    mean = sum(data) / n
-    variance = sum((x - mean) ** 2 for x in data) / n
-    std_dev = math.sqrt(variance)
-    return std_dev
+        mad = np.median(np.abs(indentation_levels - np.median(indentation_levels)))
+        return 1.0 - (mad / (np.median(indentation_levels) * 2))
